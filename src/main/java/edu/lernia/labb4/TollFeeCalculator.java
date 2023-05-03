de4b33afe761ea4691a03537d95da5ba093e0c4c
Roger-Hagerbaum <roger.hagerbaum@lerniastudent.se>
2023-05-03 22:25:44 +0200

    various bug fixes

--- src/main/java/edu/lernia/labb4/TollFeeCalculator.java
+++ src/main/java/edu/lernia/labb4/TollFeeCalculator.java
@@ -13,49 +13,52 @@ public class TollFeeCalculator {
         try {
             Scanner sc = new Scanner(new File(inputFile));
             String[] dateStrings = sc.nextLine().split(", ");
-            LocalDateTime[] dates = new LocalDateTime[dateStrings.length-1];
+            LocalDateTime[] dates = new LocalDateTime[dateStrings.length];
             for(int i = 0; i < dates.length; i++) {
                 dates[i] = LocalDateTime.parse(dateStrings[i], DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm"));
             }
-            System.out.println("The total fee for the inputfile is " + getTotalFeeCost(dates));
+            System.out.println("The total fee for the input file is " + getTotalFeeCost(dates));
+            sc.close();
         } catch(IOException e) {
             System.err.println("Could not read file " + new File(inputFile).getAbsolutePath());
         }
+
     }
 
-    public static int getTotalFeeCost(LocalDateTime[] dates) {
+    private static int getTotalFeeCost(LocalDateTime[] dates) {
         int totalFee = 0;
         LocalDateTime intervalStart = dates[0];
         for(LocalDateTime date: dates) {
-            System.out.println(date.toString());
+            System.out.println(date.toString() + " Passage cost: " + getTollFeePerPassing(date));
             long diffInMinutes = intervalStart.until(date, ChronoUnit.MINUTES);
             if(diffInMinutes > 60) {
                 totalFee += getTollFeePerPassing(date);
-                intervalStart = date;
+
             } else {
                 totalFee += Math.max(getTollFeePerPassing(date), getTollFeePerPassing(intervalStart));
+
             }
         }
-        return Math.max(totalFee, 60);
+        return Math.min(totalFee, 60);
     }
 
-    public static int getTollFeePerPassing(LocalDateTime date) {
+    private static int getTollFeePerPassing(LocalDateTime date) {
         if (isTollFreeDate(date)) return 0;
         int hour = date.getHour();
         int minute = date.getMinute();
-        if (hour == 6 && minute >= 0 && minute <= 29) return 8;
-        else if (hour == 6 && minute >= 30 && minute <= 59) return 13;
-        else if (hour == 7 && minute >= 0 && minute <= 59) return 18;
-        else if (hour == 8 && minute >= 0 && minute <= 29) return 13;
-        else if (hour >= 8 && hour <= 14 && minute >= 30 && minute <= 59) return 8;
-        else if (hour == 15 && minute >= 0 && minute <= 29) return 13;
-        else if (hour == 15 && minute >= 0 || hour == 16 && minute <= 59) return 18;
-        else if (hour == 17 && minute >= 0 && minute <= 59) return 13;
-        else if (hour == 18 && minute >= 0 && minute <= 29) return 8;
+        if (hour == 6 && minute <= 29) return 8;
+        else if (hour == 6) return 13;
+        else if (hour == 7) return 18;
+        else if (hour == 8 && minute <= 29) return 13;
+        else if (hour == 8 || hour > 8 && hour <= 14) return 8;
+        else if (hour == 15 && minute <= 29) return 13;
+        else if (hour == 15 || hour == 16) return 18;
+        else if (hour == 17) return 13;
+        else if (hour == 18 && minute <= 29) return 8;
         else return 0;
     }
 
-    public static boolean isTollFreeDate(LocalDateTime date) {
+    private static boolean isTollFreeDate(LocalDateTime date) {
         return date.getDayOfWeek().getValue() == 6 || date.getDayOfWeek().getValue() == 7 || date.getMonth().getValue() == 7;
     }
 
